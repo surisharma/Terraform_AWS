74f939fc09d572759bf1ff4fc343e057050b4436
surisharma <surinder280@gmail.com>
2024-02-22 16:00:35 +0530

    Update 03_securitygroups.tf

--- Terraform_Djanog/Terraform/03_securitygroups.tf
+++ Terraform_Djanog/Terraform/03_securitygroups.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "ecs" {
         from_port        = 0
         to_port          = 0
         protocol         = "-1"
-        security_groups      = [aws_security_group.load_balancer.id]
+        security_groups      = [aws_security_group.load-balancer.id]
     }
     ingress {
         from_port        = 22
