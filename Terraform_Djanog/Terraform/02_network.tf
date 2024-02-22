bdaa6e929fd30bdf941067befb2096584c66117f
surisharma <surinder280@gmail.com>
2024-02-22 15:59:43 +0530

    Update 02_network.tf

--- Terraform_Djanog/Terraform/02_network.tf
+++ Terraform_Djanog/Terraform/02_network.tf
@@ -102,8 +102,8 @@ resource "aws_internet_gateway" "production-igw" {
 
 # Route the public subnet traffic through the Internet Gateway
 resource "aws_route" "public-internet-igw-route" {
-    route_table_id = aws_route_table.public-route_table_id
+    route_table_id = aws_route_table.public-route-table_id
     gateway_id = aws_internet_gateway.production-igw.id
-    destination_cidr_block = "0.0.0.0"
+    destination_cidr_block = "0.0.0.0/0"
 }
 
