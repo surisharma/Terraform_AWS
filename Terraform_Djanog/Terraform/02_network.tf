c1c0050e59cd2666824b81523e2fdaa9e476800e
surisharma <surinder280@gmail.com>
2024-02-22 15:44:04 +0530

    Update 02_network.tf

--- Terraform_Djanog/Terraform/02_network.tf
+++ Terraform_Djanog/Terraform/02_network.tf
@@ -61,7 +61,7 @@ resource "aws_route_table_association" "public-route-1-association" {
 }
 
 resource "aws_route_table_association" "public-route-2-association" {
-    route_table_id = aws_route_table.public-route_table.id
+    route_table_id = aws_route_table.public-route-table.id
     subnet_id = aws_subnet.public-subnet-2.id 
 }
 
